f211bcd07e0af9f028d1bc626edf1366013ea7bd
Sara Walker <sarawall1187@gmail.com>
2019-01-18 19:14:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,6 @@ end
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-#project.css("span.location-name").text
-
+#location: project.css("span.location-name").text
+#Percent Funded: project.css("ul.project-stats li.first.funded strong").text
 create_project_hash
