9f060ba039fd0364b1042760d9bc7f1673f74cb9
Sara Walker <sarawall1187@gmail.com>
2019-01-18 19:35:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,8 @@ def create_project_hash
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
-      :location => 
+      :location => project.css("ul.project-meta span.location-name").text,
+      :
     }
   end
   projects
